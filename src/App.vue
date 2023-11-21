b62d1b4cb139c7ccfdaf31420fcf3cf98b6c9205
soboti <yoonseok.bae98@gmail.com>
2023-11-21 12:23:20 +0900

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+  <HelloWorld msg="Welcome to Your Vue.js App" class="hello"/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  /* margin-top: 60px; */
+}
+body {
+  margin: 0;
+  padding: 0;
+}
+.hello {
+  width: 100vw;
+  height: 100vh;
+  position: absolute;
+  top: 0;
+  left: 0;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,12 @@
+<template>
+  <iframe ref="ifc" src="https://pf.kakao.com/_aKxdLs" frameborder="0"></iframe>
+  <!-- <div>test</div> -->
+</template>
+
+<script setup>
+import { ref } from "vue";
+const ifc = ref(null);
+</script>
+
+<style scoped>
+</style>
